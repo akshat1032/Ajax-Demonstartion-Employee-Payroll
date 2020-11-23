9aef18b407cb3e5842168efe08446d09c0e1b640
akshat1032 <akshat.30oct@gmail.com>
2020-11-23 10:48:42 +0530

    Refactor : HTML services refactored

--- js/testService.js
+++ js/testService.js
@@ -6,7 +6,7 @@ showTime = () => {
 function makePromiseCall(methodType, url, async = true, data = null) {
     return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
+        xhr.onload = function () {
             console.log(methodType + " state change called. Ready state : ", xhr.readyState, " Status : ", xhr.status);
             if (xhr.status === 200 || xhr.status === 201) {
                 resolve(xhr.responseText);
